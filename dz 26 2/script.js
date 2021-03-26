131f3f9908b1ccc6a6b6a09ba64cf863e36a20c4
Ivan M <40141225+Bobogok@users.noreply.github.com>
2021-03-26 16:08:26 +0400

    update script
    Удалил лишние строки, изменил порядок определения функций, исправлены и доработаны незначительные недостатки

--- dz 26 2/script.js
+++ dz 26 2/script.js
@@ -1,27 +1,30 @@
-function all_lot() {
+function ama() {
 
-    function ama() {
-        var animal = document.getElementById("add_message_animal");
-        var animal_ul = document.createElement("ul");
-        animal.appendChild(animal_ul);
+    var animal = document.getElementById("add_message_animal");
+    var animal_ul = document.createElement("ul");
+    animal.appendChild(animal_ul);
 
-        var b = "";
-        for (var i = 1; i < 6; i++) { 
-            var animal_li = document.createElement("li");
-            animal.appendChild(animal_li);
-            animal_li.innerHTML = i;
-        }
-    }
-
-    function array_item() {
-        var array_item_new = new Array();
-        var random_array = [48, 33.7, true , "lion", -29 , "win", false];
-        
-        for (a = 0; a <=15; a++) {
-            var item = random_array[Math.floor(Math.random() * random_array.length)];
-        }
-        console.log(array_item_new)
+    for (var i = 1; i <= 5; i++) { 
+        var animal_li = document.createElement("li");
+        animal.appendChild(animal_li);
+        animal_li.innerHTML = 'Вложенный элемент li под номером = ' + i;
     }
 }
-window.onload = all_lot;
 
+function array_item() {
+    var array_item_new = new Array();
+    var random_array = [48, 33.7, true , "lion", -29 , "win", false];
+    
+    for (a = 0; a <= 15; a++) {
+        var item = random_array[Math.floor(Math.random() * random_array.length)];
+        array_item_new[a] = item;
+    }
+    console.log(array_item_new)
+}
+
+function onload_doc() {
+    ama()
+    array_item()
+}
+
+window.onload = onload_doc;
